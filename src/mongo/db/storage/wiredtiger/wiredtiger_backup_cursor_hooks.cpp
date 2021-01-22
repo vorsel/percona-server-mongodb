a27caf4b99da690a158065789e4abaeb9e20521b
Igor Solodovnikov <igor.solodovnikov@percona.com>
2021-01-22 20:12:34 +0200

    PSMDB-802 wiredTiger backup cursor hooks implementation

--- src/mongo/db/storage/wiredtiger/wiredtiger_backup_cursor_hooks.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_backup_cursor_hooks.cpp
@@ -29,13 +29,25 @@ Copyright (C) 2021-present Percona and/or its affiliates. All rights reserved.
     it in the license file.
 ======= */
 
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kStorage
+
 #include "mongo/db/storage/wiredtiger/wiredtiger_backup_cursor_hooks.h"
+
+#include "mongo/db/db_raii.h"
+#include "mongo/db/dbhelpers.h"
+#include "mongo/db/operation_context.h"
+#include "mongo/db/storage/encryption_hooks.h"
+#include "mongo/logv2/log.h"
+#include "mongo/util/fail_point.h"
 
 namespace mongo {
 
 namespace {
+
+MONGO_FAIL_POINT_DEFINE(backupCursorErrorAfterOpen);
+
 auto initializer(StorageEngine* storageEngine) {
-    return std::make_unique<WiredTigerBackupCursorHooks>();
+    return std::make_unique<WiredTigerBackupCursorHooks>(storageEngine);
 }
 }  // namespace
 
@@ -48,20 +60,155 @@ bool WiredTigerBackupCursorHooks::enabled() const {
 }
 
 void WiredTigerBackupCursorHooks::fsyncLock(OperationContext* opCtx) {
-    MONGO_UNREACHABLE;
+    stdx::lock_guard<stdx::mutex> lk(_mutex);
+    uassert(50885, "The node is already fsyncLocked.", _state != kFsyncLocked);
+    uassert(50884,
+            "The existing backup cursor must be closed before fsyncLock can succeed.",
+            _state != kBackupCursorOpened);
+    uassertStatusOK(_storageEngine->beginBackup(opCtx));
+    _state = kFsyncLocked;
 }
 
 void WiredTigerBackupCursorHooks::fsyncUnlock(OperationContext* opCtx) {
-    MONGO_UNREACHABLE;
+    stdx::lock_guard<stdx::mutex> lk(_mutex);
+    uassert(50888, "The node is not fsyncLocked.", _state == kFsyncLocked);
+    _storageEngine->endBackup(opCtx);
+    _state = kInactive;
 }
 
 BackupCursorState WiredTigerBackupCursorHooks::openBackupCursor(
     OperationContext* opCtx, const StorageEngine::BackupOptions& options) {
-    MONGO_UNREACHABLE;
+    stdx::lock_guard<stdx::mutex> lk(_mutex);
+    uassert(50887, "The node is currently fsyncLocked.", _state != kFsyncLocked);
+    uassert(50886,
+            "The existing backup cursor must be closed before $backupCursor can succeed.",
+            _state != kBackupCursorOpened);
+
+    // Replica sets must also return the opTime's of the earliest and latest oplog entry. The
+    // range represented by the oplog start/end values must exist in the backup copy, but are
+    // not expected to be exact.
+    repl::OpTime oplogStart;
+    repl::OpTime oplogEnd;
+
+    // If the oplog exists, capture the last oplog entry before opening the backup cursor. This
+    // value will be checked again after the cursor is established to guarantee it still exists
+    // (and was not truncated before the backup cursor was established.
+    {
+        AutoGetCollectionForRead coll(opCtx, NamespaceString::kRsOplogNamespace);
+        if (coll.getCollection()) {
+            BSONObj lastEntry;
+            uassert(50914,
+                    str::stream() << "No oplog records were found.",
+                    Helpers::getLast(
+                        opCtx, NamespaceString::kRsOplogNamespace.ns().c_str(), lastEntry));
+            auto oplogEntry = fassertNoTrace(50913, repl::OplogEntry::parse(lastEntry));
+            oplogEnd = oplogEntry.getOpTime();
+        }
+    }
+
+    // Capture the checkpointTimestamp before and after opening a cursor. If it hasn't moved,
+    // the checkpointTimestamp is known to be exact. If it has moved, uassert and have the user
+    // retry.
+    boost::optional<Timestamp> checkpointTimestamp;
+    if (_storageEngine->supportsRecoverToStableTimestamp()) {
+        checkpointTimestamp = _storageEngine->getLastStableRecoveryTimestamp();
+    };
+
+    auto filesToBackup = uassertStatusOK(_storageEngine->beginNonBlockingBackup(opCtx, options));
+    _state = kBackupCursorOpened;
+    _openCursor = UUID::gen();
+    LOGV2(29093, "Opened backup cursor", "backupId"_attr = _openCursor.get());
+
+    // A backup cursor is open. Any exception code path must leave the BackupCursorService in an
+    // inactive state.
+    auto closeCursorGuard =
+        makeGuard([this, opCtx, &lk] { _closeBackupCursor(opCtx, _openCursor.get(), lk); });
+
+    uassert(50919,
+            "Failpoint hit after opening the backup cursor.",
+            !MONGO_unlikely(backupCursorErrorAfterOpen.shouldFail()));
+
+    // Ensure the checkpointTimestamp hasn't moved. A subtle case to catch is the first stable
+    // checkpoint coming out of initial sync racing with opening the backup cursor.
+    if (checkpointTimestamp && _storageEngine->supportsRecoverToStableTimestamp()) {
+        auto requeriedCheckpointTimestamp = _storageEngine->getLastStableRecoveryTimestamp();
+        if (!requeriedCheckpointTimestamp ||
+            requeriedCheckpointTimestamp.get() < checkpointTimestamp.get()) {
+            LOGV2_FATAL(50916,
+                        "The last stable recovery timestamp went backwards. Original: "
+                        "{checkpointTimestamp} Found: {requeriedCheckpointTimestamp}",
+                        "The last stable recovery timestamp went backwards",
+                        "checkpointTimestamp"_attr = checkpointTimestamp.get(),
+                        "requeriedCheckpointTimestamp"_attr = requeriedCheckpointTimestamp);
+        }
+
+        uassert(50915,
+                str::stream() << "A checkpoint took place while opening a backup cursor.",
+                checkpointTimestamp == requeriedCheckpointTimestamp);
+    };
+
+    // If the oplog exists, capture the first oplog entry after opening the backup cursor.
+    // Ensure it is before the `oplogEnd` value.
+    if (!oplogEnd.isNull()) {
+        BSONObj firstEntry;
+        uassert(50912,
+                str::stream() << "No oplog records were found.",
+                Helpers::getSingleton(
+                    opCtx, NamespaceString::kRsOplogNamespace.ns().c_str(), firstEntry));
+        auto oplogEntry = fassertNoTrace(50918, repl::OplogEntry::parse(firstEntry));
+        oplogStart = oplogEntry.getOpTime();
+        uassert(50917,
+                str::stream() << "Oplog rolled over while establishing the backup cursor.",
+                oplogStart < oplogEnd);
+    }
+
+    auto encHooks = EncryptionHooks::get(opCtx->getServiceContext());
+    if (encHooks->enabled()) {
+        auto eseFiles = uassertStatusOK(encHooks->beginNonBlockingBackup());
+        // TODO: fix this for encryption DB
+        // filesToBackup.insert(filesToBackup.end(), eseFiles.begin(), eseFiles.end());
+    }
+
+    BSONObjBuilder builder;
+    builder << "backupId" << _openCursor.get();
+    builder << "dbpath" << storageGlobalParams.dbpath;
+    if (!oplogStart.isNull()) {
+        builder << "oplogStart" << oplogStart.toBSON();
+        builder << "oplogEnd" << oplogEnd.toBSON();
+    }
+
+    // Notably during initial sync, a node may have an oplog without a stable checkpoint.
+    if (checkpointTimestamp) {
+        builder << "checkpointTimestamp" << checkpointTimestamp.get();
+    }
+
+    Document preamble{{"metadata"_sd, builder.obj()}};
+
+    closeCursorGuard.dismiss();
+    return {_openCursor.get(), preamble, filesToBackup};
 }
 
 void WiredTigerBackupCursorHooks::closeBackupCursor(OperationContext* opCtx, const UUID& backupId) {
-    MONGO_UNREACHABLE;
+    stdx::lock_guard<stdx::mutex> lk(_mutex);
+    _closeBackupCursor(opCtx, backupId, lk);
+}
+
+void WiredTigerBackupCursorHooks::_closeBackupCursor(OperationContext* opCtx,
+                                                     const UUID& backupId,
+                                                     WithLock) {
+    uassert(50880, "There is no backup cursor to close.", _state == kBackupCursorOpened);
+    uassert(50879,
+            str::stream() << "Can only close the running backup cursor. To close: " << backupId
+                          << " Running: " << _openCursor.get(),
+            backupId == _openCursor.get());
+    _storageEngine->endNonBlockingBackup(opCtx);
+    auto encHooks = EncryptionHooks::get(opCtx->getServiceContext());
+    if (encHooks->enabled()) {
+        fassert(50934, encHooks->endNonBlockingBackup());
+    }
+    LOGV2(29092, "Closed backup cursor", "backupId"_attr = backupId);
+    _state = kInactive;
+    _openCursor = boost::none;
 }
 
 BackupCursorExtendState WiredTigerBackupCursorHooks::extendBackupCursor(OperationContext* opCtx,
